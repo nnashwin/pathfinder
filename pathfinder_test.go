ea6bdd283db41cca40d7f75cf9cdb9371ec8592f
ttymed <tyler.boright@gmail.com>
2018-04-19 23:20:51 -0700

    adds a unit test and fixtures for the pathfinder method

---
+++ pathfinder_test.go
@@ -0,0 +1,31 @@
+package pathfinder
+
+import (
+	"testing"
+)
+
+func TestDoesExistt(t *testing.T) {
+	cases := []struct {
+		path     string
+		expected bool
+		failMsg  string
+	}{
+		{
+			"./fixtures/fail.txt",
+			false,
+			"fail.txt should not be found.",
+		},
+		{
+			"./fixtures/pass.txt",
+			true,
+			"the pass.txt fixture should have been found.",
+		},
+	}
+
+	for _, c := range cases {
+		actual := DoesExist(c.path)
+		if actual != c.expected {
+			t.Error(c.failMsg)
+		}
+	}
+}
