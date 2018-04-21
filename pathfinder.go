5e84c66f09407a0c229a0e43d60ca2b64ab39c9f
Tyler Boright <lupin3.ken@gmail.com>
2018-04-20 23:34:35 -0700

    adds unit tests for the CreatePath method that can create a Dir or a File based on what path is given to it

--- pathfinder.go
+++ pathfinder.go
@@ -1,7 +1,9 @@
 package pathfinder
 
 import (
+	"log"
 	"os"
+	"path/filepath"
 )
 
 func DoesExist(path string) bool {
@@ -11,4 +13,33 @@ func DoesExist(path string) bool {
 		return false
 	}
 	return true
+}
+
+func CreatePath(path string) error {
+	if DoesExist(path) == true {
+		log.Fatal("You can not create a path that already exists.")
+	}
+
+	dir, file := filepath.Split(path)
+	if file == "" {
+		err := os.MkdirAll(path, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return nil
+	} else {
+		err := os.MkdirAll(dir, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		newFile, err := os.Create(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer newFile.Close()
+
+		return nil
+	}
 }
--- pathfinder_test.go
+++ pathfinder_test.go
@@ -1,6 +1,7 @@
 package pathfinder
 
 import (
+	"os"
 	"testing"
 )
 
@@ -27,5 +28,42 @@ func TestDoesExist(t *testing.T) {
 		if actual != c.expected {
 			t.Error(c.failMsg)
 		}
+	}
+}
+
+func TestCreatePath(t *testing.T) {
+	cases := []struct {
+		path    string
+		failMsg string
+	}{
+		{
+			"./fixtures/filedir/nestedDir2/testFile",
+			"The path created was incorrect.",
+		},
+
+		{
+			"./fixtures/filedir/seconddir/",
+			"The path created was incorrect.",
+		},
+	}
+
+	for _, c := range cases {
+		if DoesExist(c.path) != false {
+			t.Error("Cleanup on the last test was not done correct.  The path should be false before we create the file.")
+		}
+
+		err := CreatePath(c.path)
+		if err != nil {
+			t.Errorf("The following problem occurred when running CreatePath: %s", err)
+		}
+
+		if DoesExist(c.path) != true {
+			t.Error("The Create Path function didn't create a path.")
+		}
+	}
+
+	err := os.RemoveAll("./fixtures/filedir")
+	if err != nil {
+		t.Errorf("Cleanup in the CreatePath test failed with the following error: %s", err)
 	}
 }
